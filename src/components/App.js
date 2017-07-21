594f36ad276fc6df50bf3a5b3bd2d3674abdaaa9
Tammy Castro <tammycastro@gmail.com>
2017-07-20 23:05:56 -0400

    added .gitignore file updated import file

--- src/components/App.js
+++ src/components/App.js
@@ -1,98 +1,90 @@
-import React, {Component} from 'react';
-import '../styles/App.css';
-import Vehicles from './Vehicles';
-import VehicleList from './VehicleList';
-import EmptyComponent from './EmptyComponent';
-
-class Jumbotron extends Component {
-     render() {
-          return (
-            <div className="jumbotron">
-               <h1>Star Wars</h1>
-               <hr/>
-               <p>The Vehicles of Star Wars</p>
-          </div>
-          )
-     }
-}
-
-class Form extends Component {
-     render() {
-          return (
-               <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="name" value={this.props.name} onChange={this.handleNameChange}/>
-                    <button type="submit" value="Submit">Submit</button>
-               </form>
-          )
-     }
-}
-
+import React, { Component } from 'react'
+import '../styles/App.css'
 
 class App extends Component {
-  // PROPS AND STATE
-  // Set props and state below.
-  // You should set state for vehicles (empty array), value (empty string), pilot (empty) string.
-  // Enter your code below:
-  constructor(props) {
-      super(props)
-      this.state = {
-        vehicles: [],
-        value: '',
-        pilot: ''
-      }
-   }
+     constructor(props) {
+            super(props);
+            this.handleInputChange = this.handleInputChange.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
+            this.state = {
+                 vehicles: [],
+                 value: '',
+                 pilot: ''
+          }
+     }
 
+     handleInputChange(event) {
+            this.setState( { value: event.target.value });
+     }
 
-
-
-
-  // FORM: HANDLE INPUT CHANGES
-  // handleNameChange below:
-  // See form lesson for details.
-  // Enter your code below:
-handleNameChange(event) {
-     this.setState({name: event.target.value})
-}
-
-
-  //  FORM: SUBMIT METHOD
-  // handleSubmit below:
-  // See form lesson for details.
-  // Once the form is sumbited, two things need to happen: set the state of pilot to the input value.
-  // Then, set the value of the input back to an empty string.
-  // Enter your code below:
-  handleSubmit(event) {
-       event.preventDefault();
-       alert('Thank you, ' + this.state.name + ' your name was submitted');
- }
-
-  // LIFECYCLE
-  // Which lifecycle is best for fetching data?
-  // Inside this lifecycle, you will fetch the vehicles from here: https://swapi.co/api/vehicles/
-  // Once you have fetched that data, set the state of vehicles to the fetched data.
-  // In your response look for 'results'. It should return this array.
-  // You will want to use this array when you set the state of 'vehicles'. You will need this data in your render.
-  // Enter your code below:
-
-
-  // RENDER
-  // Before you can map over the data you've fetched, you will first need to store that 'state' in a variable.
-  // Map over the data.
-  // Don't forget to set the 'key'. In this case, use the vehicle name.
-  // You will need the following values: name, model, manufacturer, class, passengers, crew, length, max speed, and cargo capacity.
-  // Rendering: create a 'card' for each of the vehicles. consult the Bootstrap 4 docs for details.
-  // Enter your code below:
-
-  render() {
-    return (
-      <div className="App">
-          <Jumbotron />
-          <Form />
-          <VehicleList />
-          <EmptyComponent />
-      </div>
-    )
-  }
+     handleSubmit = (event) => {
+          event.preventDefault();
+          this.setState({ pilot: this.state.value });
+          this.setState({ value: "" });
+     }
+     componentWillMount() {
+          fetch('https://swapi.co/api/vehicles/').then((response) => {
+               return response.json()
+          }).then((data) => {
+               let vehicles = data.results
+               this.setState({
+                    vehicles
+               })
+          })
+     }
+     vehiclesMapped() {
+          return this.state.vehicles.map(vehicle => {
+               return (
+                  <div className="col-4" key={vehicle.name}>
+                  <div className="card">
+                    <div className="card-block">
+                      <h4 className="card-title">Vehicle: {vehicle.name}</h4>
+                      <p className="card-subtitle mb-2 text-muted">Model: {vehicle.model}</p>
+                    </div>
+                    <div className="rounded">
+                       <ul className="list-group list-group-flush">
+                          <li className="list-group-item">Manufacturer: {vehicle.manufacturer}</li>
+                          <li className="list-group-item">Class: {vehicle.class}</li>
+                          <li className="list-group-item">Passengers: {vehicle.passengers}</li>
+                          <li className="list-group-item">Crew: {vehicle.crew}</li>
+                          <li className="list-group-item">Length: {vehicle.length}</li>
+                          <li className="list-group-item">Max Speed: {vehicle.max_atmosphering_speed}</li>
+                          <li className="list-group-item">Cargo capacity: {vehicle.cargo_capacity}</li>
+                       </ul>
+                    </div>
+                 </div>
+                 </div>
+               )
+          })
+     }
+     render() {
+          return (
+              <div className="App">
+                    <div className="container">
+                         <div className="jumbotron">
+                         <h1 className="header">Star Wars</h1>
+                         <hr/>
+                         <p>The Vehicles of Star Wars</p>
+                         </div>
+                    </div>
+                    <div className="card">
+                         <div className="text-center card-block">
+                              <h1 className="medium">Whats your name, pilot?</h1>
+                              <form onSubmit={this.handleSubmit} className="col-4 col align-self-center">
+                                   <div className="form-group">
+                                        <input onChange={this.handleInputChange} className="form-control" type="text" placeholder="Enter your name " value={this.state.value}/>
+                                   </div>
+                                   <button type="submit" className="btn btn-primary">Submit</button>
+                              </form>
+                              <h1>{this.state.pilot}</h1>
+                         </div>
+                    </div>
+                    <div className="row">
+                         {this.vehiclesMapped()}
+                    </div>
+             </div>
+          );
+     }
 }
 
 export default App;
